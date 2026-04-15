cdca428f6745c980e7e209af516ba0ee7112542c
MamduhaNayyar <nmamduha@gmail.com>
2026-04-15 09:38:36 +0500

    changes made

---
+++ functions.py
@@ -0,0 +1,40 @@
+#fiunction def
+def greet (name):
+    print("hello", name)
+
+greet("Momo")
+#local and global variable
+y=20
+def prints ():
+     print(y)
+
+prints()
+def change():
+    global y
+    y=30
+
+change()
+prints()
+#immutable and mutable objects
+#mutable
+a=[1,2,3,4]
+b=a
+print(a)
+print(b)
+
+a.append(5)
+print(a)
+print(b)
+#immutable
+x=10
+y=x
+print(x)
+print(y)
+x=x+10
+print(x)
+print(y)
+#immutable
+text="hello"
+text="H"+text[1:]
+print(text)
+
